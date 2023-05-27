7b20fdddeebcff916e0862248d9f82008ccc6613
Ronald Palacios <95003067+RonaldPalacios@users.noreply.github.com>
2023-05-27 13:55:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,6 @@
 
 var listadoCiudades = ["New York","Tokio","Paris","Londres","Hong Kong","Moscu","EspañaS"];
 var listadoPrecio = ["$3000", "$5000", "$4780", "$7100", "$5980", "$4530"];
-var beneficios =[
-    ["Pasaje Aereo", "Traslado", "The Langham", "Seguro Medico"],
-    ["Pasaje Aereo", "Traslado", "Aman Tokyo", "Recorrido"], 
-    ["Pasaje Aereo", "Traslado", "Hôtel George-V", "Comidas Incluidas" ],
-    ["Pasaje Aereo", "Traslado", "Four Seasons Hotel", "Guia Turistica" ],
-    ["Pasaje Aereo", "Traslado", "Mandarin Oriental", "Seguro Medico" ], 
-    ["Pasaje Aereo", "Traslado", "Metropol Hotel", "Bebidas Incluidas" ]];
 
     function registrarComponentes() {
             let ciudades = document.querySelectorAll(".prising");
